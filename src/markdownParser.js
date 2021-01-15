dd9da9c3cb65c99233b480416c8688cb0440b793
Hedari <headrikun@gmail.com>
2021-01-15 17:56:37 +0300

    v2 of markdown engine

--- src/markdownParser.js
+++ src/markdownParser.js
@@ -19,24 +19,88 @@ const tag = /\`(.*)\`/gim
 const link = /\[(.*?)\]\((.*?)\)/gim
 const lineBreak = /\n$/gim
 
-// math Symbols
-const sqrt = /sqrt((\w+), (\w+))/gim
-
-export function parse(text) {
-	const result = text
-		.replace(h1, "<h1>$1</h1>")
-		.replace(h2, "<h2>$1</h2>")
-		.replace(h3, "<h3>$1</h3>")
-		.replace(h4, "<h4>$1</h4>")
-		.replace(h5, "<h5>$5</h5>")
-		.replace(h6, "<h6>$1</h6>")
-		.replace(bold, "<b>$1</b>")
+function applyMarkdown(text) {
+ return text.replace(bold, "<b>$1</b>")
 		.replace(italic, "<i>$1</i>")
 		.replace(underline, "<u>$1</u>")
 		.replace(stroke, "<del>$1</del>")
 		.replace(link, "<a href='$2'>$1</a>")
 		.replace(lineBreak, "<br />")
 		.replace(tag, "<span class='tag'>$1</span>")
-		.replace(sqrt, "√^$1{$2}")
-	return result
+}
+export function parse(text) {
+	const lines = text.split("\n")
+	let html = ""
+	let index = 0
+	while(index < lines.length) {
+		/** @type {string} */
+		const element = applyMarkdown(lines[index])
+		
+		if(element == "") {
+			html += "<br/>"
+			index++
+			continue
+		} else if(element.startsWith("#")) {
+			html += element.replace(h1, "<h1>$1</h1>")
+			.replace(h2, "<h2>$1</h2>")
+			.replace(h3, "<h3>$1</h3>")
+			.replace(h4, "<h4>$1</h4>")
+			.replace(h5, "<h5>$5</h5>")
+			.replace(h6, "<h6>$1</h6>")
+			index++
+			continue
+		} else if(element.indexOf("|") != -1) {
+			if(index + 1 < lines.length) {
+				if(lines[index + 1].indexOf("-") != -1) {
+					const headers = element.split("|")
+					index += 2
+					html += "<table> <tr>"
+					for(const header of headers) {
+						html += `<th>${header}</th>`
+					}
+					html += "</tr>"
+					while(lines[index].indexOf("|") != -1) {
+						const body = lines[index].split("|")
+						html += "<tr>"
+						for(const b of body) {
+							html += `<td>${applyMarkdown(b)}</td>`
+						}
+						html += "</tr>"
+						if(index + 1 < lines.length) {
+							index ++
+						} else {
+							break
+						}
+					}
+					html += "</table>"
+				}
+			}
+		}else if(element.startsWith("*")) {
+			html += "<ul>"
+			html += `<li>${element.replace("*", "")}</li>`
+			if(index + 1 < lines.length) {
+				index++
+				while(lines[index].startsWith("*") || lines[index].startsWith(" ")) {
+					const value = lines[index]
+					if(value.startsWith("*")) {
+						html += `<li>${value.replace("*", "")}</li>`
+					} else {
+						html = html.slice(0, -5)
+						html += `<p>${value}</p></li>`
+					}
+					if(index + 1 < lines.length) {
+						index++
+					} else {
+						console.log("happens", index)
+						break;
+					}
+				}
+			}
+			html += "</ul>"
+		} else {
+			html += `<p> ${element} </p>`
+		}
+		index++
+	}
+	return html
 }
